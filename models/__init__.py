3728c08578034a8973512dfb96aae63ba535ba36
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-07-31 16:52:15 -0700

    fix: add argument transformer dropout

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@ from .Transformer import TimeSeriesTransformer
 def args_for_model(parser, model):
     if model == 'TimeSeriesTransformer':
         parser.add_argument('--transformer_pooling', type=str, default="last", choices=['mean', 'max', 'last', 'first', 'all'])
+        parser.add_argument('--transformer_dropout', type=float, default=0.5)
         parser.add_argument('--feature_size', type=int, default=50)
         parser.add_argument('--n_head', type=int, default=5)
         parser.add_argument('--num_layers', type=int, default=1)
