91d6da728a3baecc887d7b7ce527d8c67f50342f
20183287 <20183287@kookmin.ac.kr>
2023-07-30 17:30:52 -0700

    Refactor code for better readability and organization

--- main.py
+++ main.py
@@ -1,71 +1,77 @@
+# Standard library imports
+import os
+import sys
+
+# Third party imports
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
 from torch import optim
+from tqdm import tqdm
 
+# Local application imports
 from config import get_args
-from utils import seed_everything
 from dataset import CustomDataset
-
 import models
 from loss_functions import MSE, MAE, MAPE, SMAPE
+import train
+import test
+from utils import seed_everything
+
+def prepare_data(data_path):
+    data = pd.read_csv(data_path)
+    data = data.iloc[:, 1:].values
+    return data
+
+def prepare_model(input_dim, hidden_dim, output_dim, device):
+    model = models.RNNModel(input_dim, hidden_dim, output_dim).to(device)
+    return model
+
+def run_train(train_data, train_label, valid_data, valid_label, model, batch_size, device):
+    train_data_set = CustomDataset(train_data, train_label)
+    valid_data_set = CustomDataset(valid_data, valid_label)
+
+    train_loader = DataLoader(train_data_set, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_data_set, batch_size=batch_size, shuffle=False)
+    
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    
+    # Choose loss function
+    loss_function = MSE()
+
+    trainer = train.Trainer(train_loader, valid_loader, model, loss_function, optimizer, epochs)
+    trainer.train()
+
+def run_test(test_data, model, batch_size, device):
+    test_data_set = CustomDataset(test_data, None)
+
+    test_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=False)
+
+    # Choose loss function
+    loss_function = MSE()
+
+    tester = test.Tester(test_loader, model, loss_function)
+    tester.test()
 
 if __name__ == "__main__":
     args = get_args()
-    # get args
-
     seed_everything(args.seed)
-    # fix seed
 
     device = torch.device('cuda:0')
-    # use cuda:0
+
+    model = prepare_model(args.input_dim, args.hidden_dim, args.output_dim, device)
 
     if args.mode == 'train':
-        train_data_path = pd.read_csv(args.data_path)
-        train_label_path = pd.read_csv(args.data_path)
-        valid_data_path = pd.read_csv(args.data_path)
-        valid_label_path = pd.read_csv(args.data_path)
+        train_data = prepare_data(args.train_data_path)
+        train_label = prepare_data(args.train_label_path)
+        valid_data = prepare_data(args.valid_data_path)
+        valid_label = prepare_data(args.valid_label_path)
+        
+        run_train(train_data, train_label, valid_data, valid_label, model, args.batch_size, device)
 
-        train_data = train_data_path.iloc[:, 1:].values
-        train_label = train_label_path.iloc[:, 1:].values
-        valid_data = valid_data_path.iloc[:, 1:].values
-        valid_label = valid_label_path.iloc[:, 1:].values
+    else: # args.mode == 'test'
+        test_data = prepare_data(args.test_data_path)
 
-        train_data_set = CustomDataset(train_data, train_label)
-        valid_data_set = CustomDataset(valid_data, valid_label)
-
-        train_loader = DataLoader(train_data_set, batch_size=args.batch_size, shuffle=True)
-        valid_loader = DataLoader(valid_data_set, batch_size=args.batch_size, shuffle=False)
-    else:
-        test_data_path = pd.read_csv(args.data_path)
-
-        test_data = test_data_path.iloc[:, 1:].values
-
-        test_data_set = CustomDataset(test_data, None)
-
-        test_loader = DataLoader(test_data_set, batch_size=args.batch_size, shuffle=False)
-    # load data
-
-    input_dim, hidden_dim, output_dim = args.input_dim, args.hidden_dim, args.output_dim
-    model = models.RNNModel(input_dim, hidden_dim, output_dim).to(device)
-    # build model
-
-    epoch = args.epochs
-    lr = args.lr
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    # set training hyperparameters
-
-    loss_function = MSE()
-    # loss_function = MAE()
-    # loss_function = MAPE()
-    # loss_function = SMAPE()
-    # set loss function
-
-    for epoch in range(epoch):
-        for i, (inputs, labels) in enumerate(train_loader):
-            outputs = model(inputs)
-            loss = loss_function(outputs, labels)
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        run_test(test_data, model, args.batch_size, device)
---
+++ train.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import torch
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+class Trainer():
+    def __init__(self, train_loader, valid_loader, model, loss_fn, optimizer, epochs):
+        self.train_loader = train_loader
+        self.valid_loader = valid_loader
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.epochs = epochs
+
+    def train(self):
+        train_loss_values = []
+        valid_loss_values = []
+
+        for epoch in range(self.epochs):
+            running_loss = 0.0
+
+            # Training Phase
+            self.model.train()
+            progress_bar = tqdm(enumerate(self.train_loader), total=len(self.train_loader))
+            for i, (inputs, labels) in progress_bar:
+                outputs = self.model(inputs)
+                loss = self.loss_fn(outputs, labels)
+                
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                progress_bar.set_description(f'Epoch {epoch+1}/{self.epochs} Loss: {loss.item():.4f}')
+
+            epoch_loss = running_loss / len(self.train_loader.dataset)
+            train_loss_values.append(epoch_loss)
+            print(f'\nTrain Epoch {epoch+1}/{self.epochs}, Loss: {epoch_loss:.4f}')
+
+            # Validation Phase
+            self.model.eval()
+            with torch.no_grad():
+                running_valid_loss = 0.0
+                for inputs, labels in self.valid_loader:
+                    outputs = self.model(inputs)
+                    loss = self.loss_fn(outputs, labels)
+                    running_valid_loss += loss.item() * inputs.size(0)
+                    
+                valid_loss = running_valid_loss / len(self.valid_loader.dataset)
+                valid_loss_values.append(valid_loss)
+                print(f'Validation Loss: {valid_loss:.4f}\n')
+
+        plt.figure(figsize=(10, 5))
+        plt.plot(range(1, self.epochs + 1), train_loss_values, color='blue', label='Training Loss', marker='o', linestyle='dashed', linewidth=2, markersize=6)
+        plt.plot(range(1, self.epochs + 1), valid_loss_values, color='red', label='Validation Loss', marker='o', linestyle='dashed', linewidth=2, markersize=6)
+        plt.title('Training and Validation Loss over Epochs')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
