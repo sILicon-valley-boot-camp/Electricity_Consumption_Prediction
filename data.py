05340cc73ceb0845ad28cf0860bfae57ab3b80ed
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-07-31 14:50:40 -0700

    feat: windowed time-series dataset

--- data.py
+++ data.py
@@ -1,43 +1,40 @@
-import librosa
-import numpy as np
-from PIL import Image
-import albumentations as A
+import pandas as pd
 
 import torch
 from torch.utils.data import  Dataset
 
-
-def load_data(file):
-    pass #data loading func
-
 class DataSet(Dataset):
-    def __init__(self, file_list, label=None):
-        self.file_list = file_list
+    def __init__(self, data, label, window_size, target_index):
         self.label = label
+        self.data = data.sort_values(by=['건물번호', '일시'], ignore_index=True)
+        self.drop = ['건물번호', '일시']
+        self.window_size = window_size
+        self.target_index = target_index
 
     def __len__(self):
-        return len(self.file_list)
+        return len(self.target_index)
     
     def __getitem__(self, index):
-        if self.label is None:
-            return {'data' : torch.tensor(load_data(self.file_list[index]), dtype=torch.float)}
-        else:
-            return {'data' : torch.tensor(load_data(self.file_list[index]), dtype=torch.float), 
-                    'label' : torch.tensor(load_data(self.label[index]), dtype=torch.float)}
-        
-class Preload_DataSet(Dataset):
-    def __init__(self, file_list, label=None):
-        self.file_list = file_list
-        self.label = label
+        data = self.data[self.target_index[index]-(self.window_size): self.target_index[index]+1]
+        y = data[self.label] # current time step - 11 ~ current time step, (when window_size=10)
+        x = data.iloc[1:].drop(columns = [self.label] + self.drop)  # current time step - 10 ~ current time step, (when window_size=10)
 
-        self.data = torch.stack([load_data[file] for file in file_list])
+        return {'x': torch.tensor(x.values, dtype=torch.float), #(window_size, feat_dim)
+                'y': torch.tensor(y.values, dtype=torch.float)} #(window_size+1, )
+    
+class TestDataSet(Dataset):
+    def __init__(self, data, window_size, test_start, test_end):
+        self.data = data
+        self.window_size = window_size-1
+        target_time = pd.date_range(test_start , test_end, freq='H')
+        self.target_index = self.data[self.data['일시'].isin(target_time)].index
+        self.drop = ['건물번호', '일시']
 
     def __len__(self):
-        return len(self.file_list)
+        return len(self.target_index)
     
     def __getitem__(self, index):
-        if self.label is None:
-            return {'data' : self.data[index]}
-        else:
-            return {'data' : self.data[index], 
-                    'label' : torch.tensor(load_data(self.label[index]), dtype=torch.float)}
+        data = self.data[self.target_index[index]-(self.window_size): self.target_index[index]+1]
+        x = data.drop(columns = [self.drop])  # current time step - 10 ~ current time step, (when window_size=10)
+
+        return {'x': torch.tensor(x, dtype=torch.float)}
