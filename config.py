3cd15f60a3bb27ef0aa23b2b9cb70b818e0bda6d
20183287 <20183287@kookmin.ac.kr>
2023-08-02 15:54:32 -0700

    modify_configs

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ def args_for_main(parser):
     parser.add_argument('--batch_size', type=int, default=1, help='batch_size')
 
 def args_for_train(parser):
-    parser.add_argument('--data_path', type=str, default=None, help='data path')
+    parser.add_argument('--data_path', type=str, default='data/train.csv', help='data path')
+    parser.add_argument('--info_path', type=str, default='data/building_info.csv', help='info path')
     parser.add_argument('--epochs', type=int, default=1000, help='max epochs')
     parser.add_argument('--lr', type=float, default=5e-4, help='learning rate for the optimizer')
 
@@ -18,14 +19,13 @@ def args_for_train(parser):
     parser.add_argument('--num_layers', type=int, default=1, help='num_layers')
 
 def args_for_test(parser):
-    parser.add_argument('--data_path', type=str, default=None, help='data path')
+    parser.add_argument('--data_path', type=str, default='data/test.csv', help='data path')
+    parser.add_argument('--info_path', type=str, default='data/building_info.csv', help='info path')
 
     parser.add_argument('--input_dim', type=int, default=1, help='input dimension')
     parser.add_argument('--hidden_dim', type=int, default=128, help='hidden dimension')
     parser.add_argument('--output_dim', type=int, default=1, help='output dimension')
     parser.add_argument('--num_layers', type=int, default=1, help='num_layers')
-
-
 
 def get_args():
     parser = argparse.ArgumentParser()
