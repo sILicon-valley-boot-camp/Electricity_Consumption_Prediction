f7a2eca95db4b61c73ac3cb3413110a895dc35fa
20183287 <20183287@kookmin.ac.kr>
2023-08-03 12:58:06 -0700

    modify_for_train

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import argparse
 
 def args_for_main(parser):
     parser.add_argument('--mode', type=str, default='train', help='train or test')
-    parser.add_argument('--window_size', type=int, default=10, help='window size')
+    parser.add_argument('--window_size', type=int, default=24, help='window size')
     parser.add_argument('--seed', type=int, default=42, help='seed for random')
-    parser.add_argument('--num_workers', type=int, default=0, help='num_workers')
-    parser.add_argument('--batch_size', type=int, default=1, help='batch_size')
+    parser.add_argument('--num_workers', type=int, default=4, help='num_workers')
+    parser.add_argument('--batch_size', type=int, default=64, help='batch_size')
 
 def args_for_train(parser):
     parser.add_argument('--data_path', type=str, default='data/train.csv', help='data path')
