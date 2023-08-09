8672757d3e5ca16915334348d2c1e792b265018a
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-08-09 01:27:52 -0700

    fix: cannot use deterministic algorithm
    scatter_reduce_cuda does not have a deterministic implementation

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def seed_everything(seed: int):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-    torch.use_deterministic_algorithms(True)
+    torch.use_deterministic_algorithms(True, warn_only=True)
     torch.backends.cudnn.benchmark = True
 
 def smape(true, pred):
